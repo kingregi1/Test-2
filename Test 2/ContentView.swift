4a6a314be5219a0f0a4fd683535f7452bc552aae
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-01-16 09:59:26 -1000

    Initial Commit

---
+++ Test 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test 2
+//
+//  Created by Reginald T King on 1/16/20.
+//  Copyright © 2020 Reginald T King. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
